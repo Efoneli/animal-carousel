ebf9264ab1498e78f5dbfcfa638d267ac7bb3d59
Efoneli <abbaefoneli@gmail.com>
2023-03-17 18:08:33 +0100

    today

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
+// import AliceCarousel from 'react-alice-carousel';
+// import 'react-alice-carousel/lib/alice-carousel.css';
 import axios from 'axios';
 import Image from './components/Image';
 
--- src/components/Individuals.js
+++ src/components/Individuals.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Individuals = ({image}) => {
   return (
     <div>
-        <img src={image.urls.regular} alt='unsplash image'/>
+        <img src={image.urls.full} alt='unsplash image' />
     </div>
   )
 }
